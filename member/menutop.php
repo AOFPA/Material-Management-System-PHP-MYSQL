6d671737d1009b38c4f3ae0319499a43dbcea80c
AOFPA <chalongchaith@gmail.com>
2021-12-23 16:46:45 +0700

    เพิ่มระบบสำรองข้อมูล

--- member/menutop.php
+++ member/menutop.php
@@ -21,6 +21,10 @@
         <!-- User Account Menu -->
         <li class="dropdown user user-menu">
           <!-- Menu Toggle Button -->
+          <a href="./dump.php">สำรองข้อมูล</a>
+        </li>
+        <li class="dropdown user user-menu">
+          <!-- Menu Toggle Button -->
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">
             <!-- The user image in the navbar-->
             <img src="../dist/img/profile.png" class="user-image" alt="User Image"><!-- hidden-xs hides the username on small devices so only the image appears. -->
